3ad01e29598121525d66fe365796d5eb6c1b54b1
fikaa <benidiktusvio@gmail.com>
2023-04-27 20:57:56 +0700

    🎨 style(package.json): add react-icons dependency 🎨 style(Footer.tsx): add font-bold class to some elements 🎨 style(Hero.tsx): adjust font size and add gradient background 🎨 style(NavBar.tsx): add padding to navbar and adjust button style 🎨 style(Organisasi.tsx): adjust font size and add gap to card, add text-center to some elements, add iframe to show video, and adjust button style The changes are purely cosmetic, aimed at improving the visual appearance of the application. The changes include adding the react-icons dependency to the package.json file, adjusting font sizes, adding font-bold and text-center classes to some elements, adding a gradient background to the hero section, adding padding to the navbar, adding an iframe to show a video, and adjusting button styles.
    🎨 style(Teaser.tsx): update Teaser component UI
    🎨 style(Timeline.tsx): update Timeline component UI
    🎨 style(kandidat.json): update candidate name
    🎨 style(tailwind.config.js): add custom theme
    The Teaser and Timeline components have been updated to improve the UI. The Teaser component now uses a card layout to display the video, and the Timeline component now uses a neutral background color for even numbered items. The candidate name in the kandidat.json file has been updated. A custom theme has been added to the tailwind.config.js file using the daisyui plugin.

--- src/components/Teaser.tsx
+++ src/components/Teaser.tsx
@@ -2,14 +2,19 @@ const Teaser = () => {
   return (
     <>
       <div className="flex flex-col items-center justify-center gap-8 bg-[#477167] p-4">
-        <h1 className="text-4xl font-bold tracking-[0.1em]">TEASER</h1>
-        <iframe
-          className="h-64 w-full lg:h-96 lg:w-6/12"
-          src="https://www.youtube.com/embed/_FzmYV4Ywkk/?autoplay=1&mute=1"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowFullScreen={true}
-          frameBorder={0}
-        ></iframe>
+        <h1 className="text-5xl font-bold tracking-[0.1em]">TEASER</h1>
+
+        <div className="card w-fit bg-base-100 shadow-xl flex h-84">
+          <div className="card-body">
+            <iframe
+              className="w-full aspect-video"
+              src="https://www.youtube.com/embed/bwM66NpfikM?autoplay=1&mute=1"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen={true}
+              frameBorder={0}
+            ></iframe>
+          </div>
+        </div>
       </div>
     </>
   );
--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -4,14 +4,14 @@ const Timeline = () => {
   return (
     <>
       <div className="flex flex-col items-center justify-center gap-8 bg-[#477167] p-4 pb-24 pt-24">
-        <h1 className="text-4xl font-bold tracking-[0.1em]">TIMELINE</h1>
+        <h1 className="text-5xl font-bold tracking-[0.1em]">TIMELINE</h1>
         <div className="align-center flex flex-wrap justify-center gap-4">
           {timeline.map((item) => {
             return (
               <div
                 key={item.id}
                 className={`card w-full bg-base-100 shadow-xl lg:w-96 ${
-                  item.id % 2 == 0 ? "bg-primary" : ""
+                  item.id % 2 == 0 ?"bg-neutral" : "bg-base-100" 
                 }`}
               >
                 <div className="card-body">
